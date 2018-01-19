dd4ef7105ae03a41cd31f08f7c5ac9dc6d689ca6
ivanleo94 <31164505+ivanleo94@users.noreply.github.com>
2018-01-18 18:02:23 -0700

    Update liri.js

--- liri.js
+++ liri.js
@@ -6,3 +6,45 @@
 	var twitter = require("twitter");
 	var spotify = require ("spotify");
 	var liriArgument = process.argv[2];
+//capture user input, and inform user of what to type in.
+console.log("Type my-tweets , spotify-this-song , movie-this , or do-what-it-says to get started!");
+//process[2] choses action, process[3] as search parameter for spotify or movie.
+var userCommand = process.argv[2];
+var secondCommand = process.argv[3];
+//process multiple words. Triggers if user types anything more than the above console logged options and first parameter.
+	for(i=4; i<process.argv.length; i++){
+	    secondCommand += '+' + process.argv[i];
+	}
+
+function theGreatSwitch(){
+	//action statement, switch statement to declare what action to execute.
+	switch(userCommand){
+
+		case 'my-tweets':
+		fetchTweets();
+		break;
+
+		case 'spotify-this-song':
+		spotifyMe();
+		break;
+
+		case 'movie-this':
+		aMovieForMe();
+		break;
+
+		case 'do-what-it-says':
+		followTheTextbook();
+		break;
+		
+	}
+};
+//functions/options
+function fetchTweets(){
+	console.log("Tweets headed your way!");
+	//new variable for instance of twitter, load keys from imported keys.js
+	var client = new twitter({
+		consumer_key: keys.twitterKeys.consumer_key,
+		consumer_secret: keys.twitterKeys.consumer_secret,
+		access_token_key: keys.twitterKeys.access_token_key,
+		access_token_secret: keys.twitterKeys.access_token_secret
+	});
